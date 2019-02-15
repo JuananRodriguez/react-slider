f135dbdb7060e9ce967fca3b0ba5e32db081081f
juanan <juanan@secmotic.com>
2019-02-15 19:05:18 +0100

    grouped

--- src/components/Sliders/Animations/Opacity.js
+++ src/components/Sliders/Animations/Opacity.js
@@ -11,32 +11,14 @@ export const Opacity = StyledComponent(Slide)`
        transition: opacity ${p=>p.time}ms ease-out;
     }
 
-
-    &.slide-enter {
-        /*transform: translateX(100%) translateY(100%);*/
-        /*transform: translate(100%);*/
+    &.slide-enter,
+    &.slide-exit-active,
+    &.slide-exit-done{
         opacity: 0;
-        transition: opacity ${p=>p.time}ms ease-out;
-    }
-    &.slide-enter-active {
-        opacity: 1;
-        transition: opacity ${p=>p.time}ms ease-out;
-        /*transform: translate(0%);*/
-        /*transition: transform ${p=>p.time}ms ease-out;*/
-    }
-    &.slide-exit {
-        opacity: 1;
-        transition: opacity ${p=>p.time}ms ease-out;
-        /*transform: translate(0%);*/
-    }
-    &.slide-exit-active {
-        opacity: 0;
-        transition: opacity ${p=>p.time}ms ease-out;
-        /*transform: translateX(-100%) translateY(-100%);*/
-        /*transition: transform 500ms ease-out;*/
     }
     
-    &.slide-exit-done {
-        opacity: 0;
+    &.slide-enter-active,
+    &.slide-exit{
+        opacity: 1;
     }
     `;
